3b592fc7e3de84614edcdfaf2247c1ffd92b5a24
ashleydenison <dashley9@byu.edu>
2022-03-02 14:06:23 -0700

    Final Commit

---
+++ Mission7/Views/Shared/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Mission7.Models;
+namespace Mission7.Views.Shared.Components
+{
+    public class CartSummaryViewComponent :ViewComponent
+    {
+        private Cart cart;
+
+        public CartSummaryViewComponent(Cart c)
+        {
+            cart = c;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
